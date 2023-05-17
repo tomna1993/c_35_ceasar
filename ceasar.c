7e3b0c5cabebdbe82acc5e733891cd687251974c
TomasNagy <tomi.nagy@outlook.com>
2023-05-17 17:13:03 +0200

    Add comments to string_to_int and power_of functions

--- ceasar.c
+++ ceasar.c
@@ -30,36 +30,55 @@ int main(int argc, string argv[])
 	return 0;
 }
 
+// Convert string number to integer number
 int string_to_int(string number_text)
 {
+	// Get the length of the string 
+	// Decrease it by one because strlen returns with the 
+	// number of letters in the string
+	// but the string array starts with index 0 
 	int digits = strlen(number_text) - 1;
 
+	// Declare variable to save the converted string
 	int number = 0;
 	int ten_power_of = 0;
 
+	// Iterate through the digits starting the 0th index
 	for (int i = 0; i <= digits; i++)
 	{
+		// If the character is not digit return with the number
+		// Be careful, if the string is "34x5" just the 5 will be returned 
 		if (!isdigit(number_text[digits - i]))
 		{
+			// printf("String %s is %i in decimal\n", number_text, number);
 			return number;
 		}
 		
+		// Calculate the power of 10
 		ten_power_of = power_of(10, i);
 
+		// Read the character (f.e. 8 = in ascii DEC 56)
+		// 56 - 48 = 8
+		// Digit = 8 * 10^ digit place - 1 (ones place = 10^0; tens place = 10^1)
 		number += (number_text[digits - i] - 48) * ten_power_of;
 	}
 
-	printf("String %s is %i in decimal\n", number_text, number);
+	// Debug code
+	// printf("String %s is %i in decimal\n", number_text, number);
+	// Return the converted string as integer
 	return number;
 }
 
+// Calculate given number on power of another nuber
 int power_of(int num, int powerOn)
 {
+	// A number on power of 0 is 1 
 	if (powerOn == 0)
 	{
 		return 1;
 	}
 	
+	// Declare and set a variable to calculate power of
 	int power_of_num = 1;
 	
 	for(int i = powerOn; i > 0; i--)
@@ -70,6 +89,7 @@ int power_of(int num, int powerOn)
 	return power_of_num;
 }
 
+// Encript text and return with encrypted text
 string encript_text(string plaintext)
 {
 	return plaintext;

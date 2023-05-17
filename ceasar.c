e66736bf4f485eaf3003f8fec9095e23becaf11b
TomasNagy <tomi.nagy@outlook.com>
2023-05-17 18:01:39 +0200

    Implement encription algorithm

--- ceasar.c
+++ ceasar.c
@@ -5,7 +5,7 @@
 
 int string_to_int(string number_text);
 int power_of(int num, int powerOn);
-string encript_text(string plaintext);
+void encript_text(string plaintext, const int key);
 
 int main(int argc, string argv[])
 {
@@ -22,10 +22,10 @@ int main(int argc, string argv[])
 	string plaintext = get_string("plaintext: ");
 
 	// Call function to change letters
-	string ciphertext = encript_text(plaintext);
+	encript_text(plaintext, k);
 
 	// Print ciphertext
-	printf("ciphertext: %s\n", ciphertext);
+	printf("ciphertext: %s\n", plaintext);
 
 	return 0;
 }
@@ -90,7 +90,50 @@ int power_of(int num, int powerOn)
 }
 
 // Encript text and return with encrypted text
-string encript_text(string plaintext)
+void encript_text(string plaintext, const int key)
 {
-	return plaintext;
+	// Get the length of the plaintext string
+	int length = strlen(plaintext);
+
+	// Create constant for uppercase A letter's decimal value 
+	const int uppercaseA_ASCII = 65;
+	// Create constant for lowercase A letter's decimal value
+	const int lowercaseA_ASCII = 97;
+
+	// Declare a variable c for counting the characters from 0
+	// Char A is 0, B is 1, C is 2 ...
+	int c = 0;
+
+	// Iterate through the string array
+	for (int i = 0; i < length; i++)
+	{
+		// If character is other non alphabetical character then don't change it
+		// Check if character is alphabetical and uppercase
+		if (isalpha(plaintext[i]) && isupper(plaintext[i]))
+		{
+			// Subtract 'A' decimal value from the character and add key
+			// f.e. plaintext[i] = 'A', then 'A' = 65 in ASCII, key = 1
+			// 65 - 65 + 1 = 1, then calculate the remainder when devided by 26 
+			// c = 1 
+			c = (plaintext[i] - uppercaseA_ASCII + key) % 26;	
+
+			// Add back the 'A's ASCII decimal value 
+			// plaintext[i] = 1 + 65 = 66 = 'B'
+			plaintext[i] = c + uppercaseA_ASCII;	
+		}
+		
+		// Check if character is alphabetical and lowercase
+		else if (isalpha(plaintext[i]) && islower(plaintext[i]))
+		{
+			// Subtract 'a' decimal value from the character and add key
+			// f.e. plaintext[i] = 'a', then 'a' = 97 in ASCII, key = 1
+			// 97 - 97 + 1 = 1, then calculate the remainder when devided by 26 
+			// c = 1 
+			c = (plaintext[i] - lowercaseA_ASCII + key) % 26;	
+
+			// Add back the 'a's ASCII decimal value 
+			// plaintext[i] = 1 + 97 = 98 = 'b'
+			plaintext[i] = c + lowercaseA_ASCII;	
+		}
+	}
 }

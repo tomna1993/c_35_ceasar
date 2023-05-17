84133a085592c0241b2a3ee669e19598a31773a0
TomasNagy <tomi.nagy@outlook.com>
2023-05-17 18:15:01 +0200

    Add user key checking if it contains just digits

--- ceasar.c
+++ ceasar.c
@@ -9,13 +9,25 @@ void encript_text(string plaintext, const int key);
 
 int main(int argc, string argv[])
 {
-	// Read CLA
+	// Read CLA and check if there are two argument entered by user
+	// ./build/ceasar.c key
 	if (argc != 2)
 	{
 		printf("Usage: ./build/ceasar key\n");
 		return 1;
 	}
 
+	// Check if the key given by the user contains just digits
+	for (int i = 0, length = strlen(argv[1]); i < length; i++)
+	{
+		if (!isdigit(argv[1][i]))
+		{
+			printf("Usage: ./build/ceasar key\n");
+			return 1;
+		}
+	}
+
+	// convert the key string to an integer
 	int k = string_to_int(argv[1]);
 
 	// Ask user to input plaintext
